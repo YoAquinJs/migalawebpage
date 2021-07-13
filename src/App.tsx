41f3fc8a74a7394d97c4529170233b577669b7e6
moonR2 <moonyneverend@gmail.com>
2021-07-13 00:53:52 -0500

    Now with SCSS and meta-header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Style/App.css';
+import './Style/App.scss';
 import {
     BrowserRouter as Router,
     Switch,
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,22 +1,11 @@
 import React from 'react';
-import '../Style/Home.Page.css'
+import Header from '../Components/Header' 
+
 function Home() {
     return(
-      <div className="home">
-         <div className="header"> {/* Contains the title and a short generalist description */}
-            <h1 className="title">
-                Home
-            </h1>
-             <p>
-                 This is the home page, this is some test text and I don't know what to put inside, please change me!
-             </p>
-         </div>
-          <div className="buttons"> {/* List of buttons that will open the popups. This 'popups' should be components */}
-              <button>BlockChain Team</button>
-              <button>Code Team</button>
-              <button>Random team</button>
-          </div>
-      </div>
+        <div>
+            <Header />
+        </div>
     );
 }
 export default Home;
